6e4f7dd10e2ae07333a98a5e0f2d3fcdcc3761d9
Duc Vinh Le <vinhxp95@gmail.com>
2022-05-03 22:14:32 +1000

    Finish footer

--- src/templates/HomeTemplate/Layout/Footer/Footer.js
+++ src/templates/HomeTemplate/Layout/Footer/Footer.js
@@ -1,8 +1,34 @@
 import React from 'react'
 import { Link, NavLink } from 'react-router-dom';
-import {YoutubeOutlined,FacebookOutlined,TwitterOutlined }from '@ant-design/icons'
+import { YoutubeOutlined, FacebookOutlined, TwitterOutlined } from '@ant-design/icons'
+import { useSelector } from 'react-redux';
+import _ from 'lodash'
 
-export default function Footer() {
+function Footer() {
+    const { heThongRapChieu } = useSelector(state => {
+        return state.QuanLyRapState
+    })
+    // _.map de loop qua hethongrapchiieu (la mot array cua nhieu Obj <=> hethongRap la opjec)
+    const arrCumRap = _.map(heThongRapChieu, (hethongRap) => {
+        // he thong rap la 1 object 
+        // {"lstCumRap": [...]
+        //     "maHeThongRap": "BHDStar",
+        //     //     "tenHeThongRap": "BHD Star Cineplex",
+        //     //     "logo": "http://movieapi.cyberlearn.vn/hinhanh/bhd-star-cineplex.png",
+        //     //     "mahom": "GP00"
+        // }
+        // 
+        return _.pick(hethongRap, ['maHeThongRap', "tenHeThongRap", "logo", "mahom"]);
+    })
+    console.log("arrCumRap", arrCumRap);
+    const renderPartner = () => {
+        return arrCumRap?.map((rapFilm, index) => {
+            return <li  key={index} className='ml-5'>
+                <img className='rounded-full cursor-pointer' width="50" src={rapFilm.logo} alt={rapFilm.tenHeThongRap} />
+            </li>
+        })
+
+    }
     return (
         <footer className="px-4 py-8 dark:bg-coolGray-800 dark:text-coolGray-400 bg-gray-700 text-white ">
             <div className="container px-10 flex flex-wrap items-center justify-center mx-auto space-y-4 sm:justify-between sm:space-y-0">
@@ -21,15 +47,28 @@ export default function Footer() {
                         </li>
                     </ul>
                 </div>
+                {/* RENDER PARTNERS */}
+                <div className='Partners'>
+                    <ul className="flex flex-wrap items-center mb-0">
+                        {/* <li className='ml-5'>
+                            <NavLink to="/home" className="text-white">Terms of Use</NavLink>
+                        </li>
+                        <li className='ml-5'>
+                            <NavLink to="#" className="text-white">Policy</NavLink>
+                        </li> */}
+                        {renderPartner()}
+                    </ul>
+                </div>
+                {/* RENDER SOCIAL MEDIA */}
                 <ul className="flex flex-wrap pl-3 space-x-4 sm:space-x-8">
                     <li>
-                        <NavLink to="#" className="text-white"><YoutubeOutlined style={{"fontSize":"2.15rem"}}/></NavLink>
+                        <NavLink to="#" className="text-white"><YoutubeOutlined style={{ "fontSize": "2.15rem" }} /></NavLink>
                     </li>
                     <li>
-                    <NavLink to="#" className="text-white"><FacebookOutlined style={{"fontSize":"2.15rem"}}/></NavLink>
+                        <NavLink to="#" className="text-white"><FacebookOutlined style={{ "fontSize": "2.15rem" }} /></NavLink>
                     </li>
                     <li>
-                    <NavLink to="#" className="text-white"><TwitterOutlined style={{"fontSize":"2.15rem"}}/></NavLink>
+                        <NavLink to="#" className="text-white"><TwitterOutlined style={{ "fontSize": "2.15rem" }} /></NavLink>
                     </li>
                 </ul>
             </div>
@@ -37,3 +76,5 @@ export default function Footer() {
 
     )
 }
+
+export default React.memo(Footer);
